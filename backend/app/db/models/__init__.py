462761fdf8710adf2cbcb40ccb6383aaf609d512
F8Y <batigoal277@gmail.com>
2026-01-14 23:19:46 +1000

    add: entry point to models

---
+++ backend/app/db/models/__init__.py
@@ -0,0 +1,7 @@
+from .client import Client
+from .loan import Loan
+from .deposit import Deposit
+from .deposit_type import DepositType
+from .job import Job
+from .education import EducationLevel
+from .marital_status import MaritalStatus
