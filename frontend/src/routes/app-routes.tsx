3320f774fc9b4a24f22d65ccbb0425614f62a372
F8Y <batigoal277@gmail.com>
2026-01-31 19:26:14 +1000

    add: routing entry point

--- frontend/src/routes/app-routes.tsx
+++ frontend/src/routes/app-routes.tsx
@@ -1,9 +1,15 @@
 import { Routes, Route } from 'react-router-dom';
+import { MainLayout } from '@/layouts/main-layout';
+import { DashboardPage, ClientsListPage, ClientDetailPage } from '@pages/index';
 
 export const AppRoutes = () => {
     return (
         <Routes>
-
+            <Route element={<MainLayout />}>
+                <Route path="/" element={<DashboardPage />} />
+                <Route path="/clients" element={<ClientsListPage />} />
+                <Route path="/clients/:id" element={<ClientDetailPage />} />
+            </Route>
         </Routes>
     );
 };
