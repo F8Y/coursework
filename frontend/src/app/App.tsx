7f4b13eb35c234cde6a4d44fda0e261c602dfffb
F8Y <batigoal277@gmail.com>
2026-01-31 19:20:16 +1000

    edit: main file with routes rendering

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,14 +1,11 @@
-
 import { BrowserRouter } from 'react-router-dom';
+import { AppRoutes } from '@/routes/app-routes';
 import './styles/globals.scss';
 
 export const App = () => {
     return (
         <BrowserRouter>
-            <div>
-                <h1>Bank Management System</h1>
-                <p>Frontend initialized with TypeScript, Sass, and FSD.</p>
-            </div>
+            <AppRoutes />
         </BrowserRouter>
     );
 };
